bfb3202090a3a7163ba8930036b8811ddbb5f8dc
mick-roper <mick@theropers.co.uk>
2020-06-12 13:20:02 +0100

    fixed a typo

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -7,7 +7,7 @@ impl<'a> System<'a> for VisibilitySystem {
     type SystemData = (WriteStorage<'a, Viewshed>, WriteStorage<'a, Position>);
 
     fn run(&mut self, (mut viewshed, pos): Self::SystemData) {
-        for (viewshed, pos) in (&mut viewshed, pos).join() {
+        for (viewshed, pos) in (&mut viewshed, &pos).join() {
             
         }
     }
