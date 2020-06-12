96f1b4b6ca53c3f9346bcd2459e4cd1c287204e1
mick-roper <mick@theropers.co.uk>
2020-06-12 08:49:50 +0100

    added map generation

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,6 @@ struct Renderable {
 }
 
 #[derive(Component)]
-struct LetftMover {}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LetftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
-#[derive(Component)]
 struct Player {}
 
 struct State {
@@ -49,6 +31,9 @@ impl GameState for State {
 
         self.run_systems();
 
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -60,9 +45,7 @@ impl GameState for State {
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
-        self.ecs.maintain();
+        // run systems in here
     }
 }
 
@@ -81,9 +64,10 @@ fn main() {
         ecs: World::new()
     };
 
+    gs.ecs.insert(new_map());
+
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LetftMover>();
     gs.ecs.register::<Player>();
 
     gs.ecs
@@ -97,31 +81,22 @@ fn main() {
         .with(Player{})
         .build();
 
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LetftMover{})
-            .build();
-    }
-
-    
-
     rltk::main_loop(context, gs);
 }
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let new_x = pos.x + delta_x;
+        let new_y = pos.y + delta_y;
+        let destination_idx = xy_idx(new_x, new_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, new_x));
+            pos.y = min(49, max(0, new_y));
+        }
     }
 }
 
@@ -155,7 +130,7 @@ fn new_map() -> Vec<TileType> {
         map[xy_idx(79, y)] = TileType::Wall;
     }
 
-    let mut rng = ::rltk::RandomNumberGenerator::new();
+    let mut rng = rltk::RandomNumberGenerator::new();
 
     for _i in 0..400 {
         let x = rng.roll_dice(1, 79);
@@ -167,4 +142,32 @@ fn new_map() -> Vec<TileType> {
     }
 
     map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+
+    let floor_colour = RGB::from_f32(0.5, 0.5, 0.5);
+    let wall_colour = RGB::from_f32(0.5, 0.5, 0.5);
+    let black = RGB::from_f32(0., 0., 0.);
+    let dot = rltk::to_cp437('.');
+    let hash = rltk::to_cp437('#');
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, floor_colour, black, dot);
+            },
+            TileType::Wall => {
+                ctx.set(x, y, wall_colour, black, hash);
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
 }
