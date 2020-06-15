7dcd465722607b1cab039f98e8b330ba1a88fc9b
mick-roper <mick@theropers.co.uk>
2020-06-15 08:47:08 +0100

    added dirty tracking to viewsheds

--- src/player.rs
+++ src/player.rs
@@ -1,20 +1,23 @@
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
-use super::{Position, Player, TileType, State, Map};
+use super::{Position, Player, TileType, State, Map, Viewshed};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
     let map = ecs.fetch::<Map>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
         let new_x = pos.x + delta_x;
         let new_y = pos.y + delta_y;
         let destination_idx = map.xy_idx(new_x, new_y);
         if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, new_x));
             pos.y = min(49, max(0, new_y));
+
+            viewshed.dirty = true;
         }
     }
 }
