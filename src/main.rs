17fcefbe401b2c1ff7cd875222bf6f3f2a2b6cf8
mick-roper <mick@theropers.co.uk>
2020-10-12 08:49:41 +0100

    added entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rltk::{Console, GameState, Rltk};
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world")
+    }
+}
+
 fn main() {
-    println!("Hello, world!")
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50().with_title("Star Rogue").build();
+
+    let gs = State {};
+    rltk::main_loop(context, gs);
 }
