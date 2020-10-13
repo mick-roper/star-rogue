863d7ae3854a9e9994ca307c9ca455f5b941b2e4
mick-roper <mick.roper@dazn.com>
2020-10-13 14:58:40 +0100

    more refactoring

--- src/map.rs
+++ src/map.rs
@@ -50,17 +50,17 @@ impl Map {
             }
 
             if ok {
-                apply_room_to_map(&new_room, &mut map);
+                map.apply_room_to_map(&new_room);
 
                 if !map.rooms.is_empty() {
                     let (new_x, new_y) = new_room.centre();
                     let (prev_x, prev_y) = map.rooms[map.rooms.len() - 1].centre();
                     if rng.range(0, 2) == 1 {
-                        apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
-                        apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                        map.apply_horizontal_tunnel(prev_x, new_x, prev_y);
+                        map.apply_vertical_tunnel(prev_y, new_y, new_x);
                     } else {
-                        apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
-                        apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                        map.apply_vertical_tunnel(prev_y, new_y, prev_x);
+                        map.apply_horizontal_tunnel(prev_x, new_x, new_y);
                     }
                 }
 
@@ -71,6 +71,32 @@ impl Map {
         map
     }
 
+    fn apply_room_to_map(&mut self, room: &Rect) {
+        for y in room.y1 +1 ..= room.y2 {
+            for x in room.x1 + 1 ..= room.x2 {
+                self.tiles[xy_idx(self.width, x, y)] = TileType::Floor;
+            }
+        }
+    }
+    
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        for x in min(x1, x2) ..= max(x1, x2) {
+            let idx = xy_idx(self.width, x, y);
+            if idx > 0 && idx < (self.width * self.height) as usize {
+                self.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+    
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        for y in min(y1, y2) ..= max(y1, y2) {
+            let idx = xy_idx(self.width, x, y);
+            if idx > 0 && idx < (self.width * self.height) as usize {
+             self.tiles[idx] = TileType::Floor;
+            }
+        }
+    }
+
     pub fn get_tile(&self, x: i32, y: i32) -> TileType {
         self.tiles[xy_idx(self.width, x, y)]
     }
@@ -78,30 +104,4 @@ impl Map {
 
 fn xy_idx(width: i32, x: i32, y: i32) -> usize {
     (y * width + x) as usize
-}
-
-fn apply_room_to_map(room: &Rect, map: &mut Map) {
-    for y in room.y1 +1 ..= room.y2 {
-        for x in room.x1 + 1 ..= room.x2 {
-            map.tiles[xy_idx(map.width, x, y)] = TileType::Floor;
-        }
-    }
-}
-
-fn apply_horizontal_tunnel(map: &mut Map, x1: i32, x2: i32, y: i32) {
-    for x in min(x1, x2) ..= max(x1, x2) {
-        let idx = xy_idx(map.width, x, y);
-        if idx > 0 && idx < (map.width * map.height) as usize {
-            map.tiles[idx] = TileType::Floor;
-        }
-    }
-}
-
-fn apply_vertical_tunnel(map: &mut Map, y1: i32, y2: i32, x: i32) {
-    for y in min(y1, y2) ..= max(y1, y2) {
-        let idx = xy_idx(map.width, x, y);
-        if idx > 0 && idx < (map.width * map.height) as usize {
-            map.tiles[idx] = TileType::Floor;
-        }
-    }
 }
