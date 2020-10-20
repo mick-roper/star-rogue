67962aabf00c169f8fae217b41255c81e9ecf2e2
mick-roper <mick.roper@dazn.com>
2020-10-20 13:18:03 +0100

    added code to insert potions into the map

--- src/spawner.rs
+++ src/spawner.rs
@@ -39,12 +39,12 @@ pub fn random_monster(ecs: &mut World, x: i32, y: i32) {
 
 pub fn spawn_room(ecs: &mut World, map_width: i32, room: &Rect) {
     let mut monster_spawn_points: Vec<i32> = Vec::new();
-    // let mut item_spawn_points: Vec<usize> = Vec::new();
+    let mut item_spawn_points: Vec<i32> = Vec::new();
 
     { // this scope keeps the borrow checker happy
         let mut rng = ecs.write_resource::<RandomNumberGenerator>();
         let num_monsters = rng.roll_dice(1, MAX_MONSTERS_PER_ROOM + 2) - 3;
-        let num_items = rng.roll_dice(1, MAX_MONSTERS_PER_ROOM + 2) - 3;
+        let num_items = rng.roll_dice(1, MAX_ITEMS_PER_ROOM + 2) - 3;
 
         for _i in 0..num_monsters {
             let mut added = false;
@@ -58,12 +58,31 @@ pub fn spawn_room(ecs: &mut World, map_width: i32, room: &Rect) {
                 }
             }
         }
+
+        for _i in 0..num_items {
+            let mut added = false;
+            while !added {
+                let x = room.x1 + rng.roll_dice(1, i32::abs(room.x2 - room.x1));
+                let y = room.y1 + rng.roll_dice(1, i32::abs(room.y2 - room.y1));
+                let idx = y * map_width + x;
+                if !item_spawn_points.contains(&idx) {
+                    item_spawn_points.push(idx);
+                    added = true;
+                }
+            }
+        }
     }
 
     for idx in monster_spawn_points.iter() {
         let x = *idx % map_width;
         let y = *idx / map_width;
         random_monster(ecs, x as i32, y as i32);
+    }
+
+    for idx in item_spawn_points.iter() {
+        let x = *idx % map_width;
+        let y = *idx / map_width;
+        health_potion(ecs, x as i32, y as i32);
     }
 }
 
